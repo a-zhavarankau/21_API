5849ea443c87519744746ec491ef7a9993d563e6
Sasha <Sasha@Nastassias-MacBook-Pro.local>
2022-10-08 01:50:49 +0200

    Make changes in templates, urls and views (3.11)

--- task_api/company/urls.py
+++ task_api/company/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from . import views
 
+app_name = 'company'
 urlpatterns = [
-    path('', views.hellopage),
-    path('department/', views.deps),
-    path('department/<int:dep_id>', views.dep_by_id),
-    path('employee/<int:empl_id>', views.empl),
-    path('project/<int:proj_id>', views.proj)
+    path('', views.company, name='index'),
+    path('department/', views.deps, name='deps'),
+    path('department/<int:dep_id>', views.dep_by_id, name='dep_by_id'),
+    path('employee/<int:empl_id>', views.empl, name='empl_by_id'),
+    path('project/<int:proj_id>', views.proj, name='proj_by_id')
 ]
--- task_api/company/views.py
+++ task_api/company/views.py
@@ -1,17 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from django.http import Http404
 from company.models import Department, Employee, Project
 
 
-def hellopage(request):
-    all_deps = ", ".join([f"{d.name} (id={d.id})" for d in Department.objects.all()])
-    all_empls = ", ".join([f"{e.name} (id={e.id})" for e in Employee.objects.all()])
-    all_projs = ", ".join([f"{p.name} (id={p.id})" for p in Project.objects.all()])
-    return HttpResponse(f"<h3>Hi there! It's company's main page. <br>Company consists of 3 tables:<h4><li>Department: {all_deps}</li><li>Employee: {all_empls}</li><li>Project: {all_projs}</li></h4></h3>")
+def company(request):
+    template = 'company/index.html'
+    return render(request, template)
 
 def deps(request):
-    # all_deps = ", ".join([f"{d.name} (id={d.id})" for d in Department.objects.all()])
     all_deps = Department.objects.all()
     context = {'departments_list': all_deps, }
     template = 'company/departments.html'
@@ -19,19 +16,11 @@ def deps(request):
 
 
 def dep_by_id(request, dep_id):
-    try:
-        dep_id_values = Department.objects.values().get(id=dep_id)
-        dep_id_info_list = [f"{k}: {v}" for k, v in dep_id_values.items()]
-    except Department.DoesNotExist:
-        raise Http404("Department does not exist")
-    else:
-        context = {'department_info': dep_id_info_list, }
+    dep_by_id = get_object_or_404(Department, id=dep_id)
+    context = {'department': dep_by_id, }
     template = 'company/department_info.html'
     return render(request, template, context)
 
-
-# return HttpResponse(f"You want to see info about department #{dep_id}:   {dep_id_info}")
-    # dep_id_info = ", ".join([str(i) for i in Department.objects.filter(id=dep_id).values()])
 
 def empl(request, empl_id):
     return HttpResponse(f"You want to see info about employee #{empl_id}")
